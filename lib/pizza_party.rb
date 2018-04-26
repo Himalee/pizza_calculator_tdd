00008c6be3da951128632118ce775b92665a9415
Himalee <himalee.tailor@gmail.com>
2018-04-26 16:52:00 +0100

    Testing output

--- lib/pizza_party.rb
+++ lib/pizza_party.rb
@@ -1,12 +1,42 @@
 class PizzaParty
 
+  def initialize(output = $stdout)
+    @output = output
+  end
+
+  def people_prompt
+    @output.puts "How many people are there?"
+  end
+
+  def pizza_prompt
+    @output.puts "How many pizzas are there?"
+  end
+
+  def slices_prompt
+    @output.puts "How many slices does each pizza have?"
+  end
+
   def calculate_portions(people, pizza, slices)
         total_slices = pizza * slices
         [total_slices / people, total_slices % people]
-
   end
 
 end
+
+
+# class InputOutput
+#
+#   def initialize(people, pizza, slices)
+#     @people = people
+#     @pizza = pizza
+#     @slices = slices
+#   end
+#
+#   def print_to_stdout
+#     people.gets
+#     pizza.gets
+#     slices.gets
+#   end
 
 #def initialize
 
--- spec/pizza_party_spec.rb
+++ spec/pizza_party_spec.rb
@@ -1,6 +1,12 @@
 require "pizza_party"
 
 describe PizzaParty do
+
+  before(:each) do
+    @output = StringIO.new
+    @pizza_party = PizzaParty.new(@output)
+  end
+
   describe "#calculate_portions" do
       context "one person, one pizza, one slice" do
         it "returns 1" do
@@ -24,6 +30,27 @@ describe PizzaParty do
         it "returns 2" do
           expect(subject.calculate_portions(8, 2, 5)).to be == [1, 2]
         end
+      end
+    end
+
+    describe "#people_prompt" do
+      it "displays prompt for number of people" do
+        @pizza_party.people_prompt
+        expect(@output.string).to eq("How many people are there?\n")
+      end
+    end
+
+    describe "#pizza_prompt" do
+      it "displays prompt for number of pizzas" do
+        @pizza_party.pizza_prompt
+        expect(@output.string).to eq("How many pizzas are there?\n")
+      end
+    end
+
+    describe "#slices_prompt" do
+      it "displays prompt for number of slices" do
+        @pizza_party.slices_prompt
+        expect(@output.string).to eq("How many slices does each pizza have?\n")
       end
     end
 
